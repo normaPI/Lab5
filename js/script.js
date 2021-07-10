24577e55afdbf855522054aad19cf35beaea321e
normaPI <73075667+normaPI@users.noreply.github.com>
2021-07-10 16:16:38 -0500

    (Feat: Juery)

---
+++ js/script.js
@@ -0,0 +1,31 @@
+
+$("#ButtonPost").on("click",function(e){
+    //alert("click con JQuery")
+    e.preventDefault();
+    var lab = $("#todoText").val();
+
+    $('#todoList').append(
+        $(document.createElement('input')).prop({
+            id: 'myCheckBox',
+            name: 'ToDos',
+            type: 'checkbox'
+        })
+    ).append(
+        $(document.createElement('label')).prop({
+            for: 'myCheckBox'
+        }).html(lab)
+        ).append(document.createElement('br'));
+
+});
+
+$("#ButtonClear").on("click",function(){
+    $("[name='ToDos']").prop('checked', false);  
+});
+
+$("#ButtonMark").on("click",function(){
+    $("[name='ToDos']").prop('checked', true);  
+});
+
+$("#ButtonDelete").on("click",function(){
+    $('#todoList').empty();
+});
